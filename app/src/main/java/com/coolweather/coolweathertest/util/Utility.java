262bfb73674094445c532cc5b1038f789a50ad9c
zchangzhi <2647120972@qq.com>
2022-06-07 16:44:32 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/coolweather/coolweathertest/util/Utility.java
+++ app/src/main/java/com/coolweather/coolweathertest/util/Utility.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import com.coolweather.coolweathertest.db.City;
 import com.coolweather.coolweathertest.db.County;
 import com.coolweather.coolweathertest.db.Province;
+import com.coolweather.coolweathertest.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -66,5 +68,17 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    public static Weather handleWeatherResponse(String response){
+        try {
+            JSONObject jsonObject=new JSONObject(response);
+            JSONArray jsonArray=jsonObject.getJSONArray("HeWeather");
+            String weatherContent=jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent,Weather.class);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
